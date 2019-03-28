082e3c7414d75e42395454753fbd423a3bc2afc0
Jacob Diaz <jdiaz21@lsu.edu>
2019-03-28 09:53:07 -0500

    configuring for heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 #import django-heroku
+import django_heroku # <- this line goes near the top of the file
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -120,3 +121,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #django_heroku.settings(locals()) # <- this line should be a the end of the file
+# Activate Django-Heroku.
+django_heroku.settings(locals()) # <- this line should be a the end of the file
